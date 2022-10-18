e006c4bc39862b3971fdbb9ca345e1e200047e95
Nikolay <nikolay.batovkin@gmail.com>
2022-10-18 13:23:48 +0300

    VIS-14

--- lib/pages/profile/profile_edit.dart
+++ lib/pages/profile/profile_edit.dart
@@ -20,6 +20,8 @@ class _ProfileEditState extends State<ProfileEdit> {
   final DBController _dbController = DBController.instance;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final List<String> _dropdownItems = ['male', 'female'];
+  late String _dropdownValue;
 
   _ProfileEditState(this.sportsman);
 
@@ -29,6 +31,7 @@ class _ProfileEditState extends State<ProfileEdit> {
 
     _nameController.text = sportsman.firstName;
     _phoneController.text = sportsman.phone;
+    _dropdownValue = sportsman.gender ? _dropdownItems[0] : _dropdownItems[1];
   }
 
   @override
@@ -42,17 +45,61 @@ class _ProfileEditState extends State<ProfileEdit> {
           padding: const EdgeInsets.symmetric(horizontal: 15).copyWith(top: 20),
           child: Column(
               children: [
-                TextField(
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                  controller: _nameController,
+                Row(
+                  children: [
+                    const SizedBox(
+                        width: 60,
+                        child: Text('Name:', style: TextStyle(fontSize: 16))
+                    ),
+                    Flexible(
+                      child: TextField(
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                        controller: _nameController,
+                      ),
+                    ),
+                  ],
                 ),
-                TextField(
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                  controller: _phoneController,
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 60,
+                      child: Text('Phone:', style: TextStyle(fontSize: 16))
+                    ),
+                    Flexible(
+                      child: TextField(
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                        controller: _phoneController,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                        width: 60,
+                        child: Text('Gender:', style: TextStyle(fontSize: 16))
+                    ),
+                    DropdownButton(
+                      elevation: 2,
+                      value: _dropdownValue,
+                      items: _dropdownItems.map((String item) {
+                        return DropdownMenuItem(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _dropdownValue = newValue!;
+                          sportsman.gender = newValue == 'male';
+                        });
+                      },
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 20),
                 Center(
