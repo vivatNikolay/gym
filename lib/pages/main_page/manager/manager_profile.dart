8c8ed46572978580b392734f39cf2d1463b6e1a1
Nikolay <nikolay.batovkin@gmail.com>
2023-01-15 20:54:23 +0300

    rework back button in manager profile

--- lib/pages/main_page/manager/manager_profile.dart
+++ lib/pages/main_page/manager/manager_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:sportmen_in_gym/pages/home.dart';
 
 import '../../../controllers/visit_http_controller.dart';
 import '../../../controllers/account_http_controller.dart';
@@ -36,149 +37,155 @@ class _ManagerProfileState extends State<ManagerProfile> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sportsman'),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Theme.of(context).backgroundColor,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(10.0),
-          child: FutureBuilder<Account>(
-              future: _futureAccount,
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return const Center(
-                      child: CircularProgressIndicator(
-                          color: mainColor, strokeWidth: 5),
-                    );
-                  default:
-                    if (snapshot.hasError) {
-                      return noConnectionMess();
-                    }
-                    return Container(
-                      padding: const EdgeInsets.all(2),
-                      margin: const EdgeInsets.symmetric(vertical: 2),
-                      height: 210,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(15.0)),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                flex: 4,
-                                child: CircleAvatar(
-                                  backgroundColor:
-                                      Theme.of(context).backgroundColor,
-                                  radius: 50.0,
-                                  child: Image.asset(
-                                      'images/profileImg${snapshot.data!.iconNum}.png'),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 7,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Tooltip(
-                                      message: snapshot.data!.firstName,
-                                      child: Text(snapshot.data!.firstName,
-                                          maxLines: 2,
-                                          style: const TextStyle(
-                                              fontSize: 28,
-                                              fontWeight: FontWeight.bold)),
-                                    ),
-                                    Tooltip(
-                                      message: snapshot.data!.email,
-                                      child: Text(snapshot.data!.email,
-                                          maxLines: 2,
-                                          style: const TextStyle(fontSize: 19)),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton.icon(
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: const Size.fromWidth(140),
-                                  backgroundColor: mainColor,
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10),
-                                    ),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pushNamedAndRemoveUntil(context, 'home', (route) => false);
+        return Future.value(true);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sportsman'),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Theme.of(context).backgroundColor,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(10.0),
+            child: FutureBuilder<Account>(
+                future: _futureAccount,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const Center(
+                        child: CircularProgressIndicator(
+                            color: mainColor, strokeWidth: 5),
+                      );
+                    default:
+                      if (snapshot.hasError) {
+                        return noConnectionMess();
+                      }
+                      return Container(
+                        padding: const EdgeInsets.all(2),
+                        margin: const EdgeInsets.symmetric(vertical: 2),
+                        height: 210,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(15.0)),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: CircleAvatar(
+                                    backgroundColor:
+                                        Theme.of(context).backgroundColor,
+                                    radius: 50.0,
+                                    child: Image.asset(
+                                        'images/profileImg${snapshot.data!.iconNum}.png'),
                                   ),
                                 ),
-                                onPressed: () async {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) => ManagerProfileEdit(
-                                          account: snapshot.data!, isEdit: true)));
-                                },
-                                icon: const Icon(Icons.edit),
-                                label: const Text("Edit profile"),
-                              ),
-                              const SizedBox(width: 20),
-                              ElevatedButton.icon(
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: const Size.fromWidth(140),
-                                  backgroundColor: mainColor,
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10),
-                                    ),
+                                Expanded(
+                                  flex: 7,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Tooltip(
+                                        message: snapshot.data!.firstName,
+                                        child: Text(snapshot.data!.firstName,
+                                            maxLines: 2,
+                                            style: const TextStyle(
+                                                fontSize: 28,
+                                                fontWeight: FontWeight.bold)),
+                                      ),
+                                      Tooltip(
+                                        message: snapshot.data!.email,
+                                        child: Text(snapshot.data!.email,
+                                            maxLines: 2,
+                                            style: const TextStyle(fontSize: 19)),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                onPressed: () async {
-                                  bool success = await _visitHttpController
-                                      .addVisitToSportsman(snapshot.data!);
-                                  if (success) {
-                                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                        content: Text('Visit added')));
-                                  }
-                                },
-                                icon: const Icon(Icons.add),
-                                label: const Text("Add visit"),
-                              ),
-                            ],
-                          ),
-                          ElevatedButton.icon(
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: const Size.fromWidth(300),
-                              backgroundColor: mainColor,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
+                              ],
                             ),
-                            onPressed: isMembershipInactive(
-                                    snapshot.data!.subscriptions)
-                                ? () {
-                              Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context) => AddMembership(snapshot.data!.email)));
-                            }
-                                : null,
-                            icon: const Icon(Icons.add_card),
-                            label: const Text("Add new membership"),
-                          ),
-                        ],
-                      ),
-                    );
-                }
-              }),
+                            const SizedBox(height: 4),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                    fixedSize: const Size.fromWidth(140),
+                                    backgroundColor: mainColor,
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () async {
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (context) => ManagerProfileEdit(
+                                            account: snapshot.data!, isEdit: true)));
+                                  },
+                                  icon: const Icon(Icons.edit),
+                                  label: const Text("Edit profile"),
+                                ),
+                                const SizedBox(width: 20),
+                                ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                    fixedSize: const Size.fromWidth(140),
+                                    backgroundColor: mainColor,
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () async {
+                                    bool success = await _visitHttpController
+                                        .addVisitToSportsman(snapshot.data!);
+                                    if (success) {
+                                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                          content: Text('Visit added')));
+                                    }
+                                  },
+                                  icon: const Icon(Icons.add),
+                                  label: const Text("Add visit"),
+                                ),
+                              ],
+                            ),
+                            ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size.fromWidth(300),
+                                backgroundColor: mainColor,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                              ),
+                              onPressed: isMembershipInactive(
+                                      snapshot.data!.subscriptions)
+                                  ? () {
+                                Navigator.of(context).push(
+                                    MaterialPageRoute(builder: (context) => AddMembership(snapshot.data!.email)));
+                              }
+                                  : null,
+                              icon: const Icon(Icons.add_card),
+                              label: const Text("Add new membership"),
+                            ),
+                          ],
+                        ),
+                      );
+                  }
+                }),
+          ),
         ),
       ),
     );
